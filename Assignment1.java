f8df0bdd31349579460c36d364df01b5c877fb74
Abdulmohsenalayaife <91384891+Abdulmohsenalayaife@users.noreply.github.com>
2021-09-26 12:10:58 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+int a = 3.5;
+int b = 11.25;
+
+System.Out.print (a + b)  ;  
+
+  System.Out.print(a – b)  ; 
+
+  System.Out.print(a * b) ;
+
+  System.Out.print(a / b)  ;  
+
+  System.Out.print(a % b)  ;  
+
+  System.Oyt.Print(a > b) ;
+
+  System.Oyt.Print(a >= b) ;
+
+  System.Oyt.Print(a < b) ;
+
+  System.Oyt.Print(a <= b) ;
+
+  System.Oyt.Print(a != b) ;
+  
+  System.Oyt.Print(a == b) ;
+
+//Your Name Abdulmohsen Ali Ahmed Alayafi
+//Your ID 442002918
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      
+  }
+}
