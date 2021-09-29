2f289480dec499ed581962160ade14be0257358c
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 12:48:54 +0300

    تاكد من عمل الملاحظات

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
+//your grade is 2/5
 
+/*
+1- put the code inside the main
+2- let the user enter the number from the scanner
+3- use the type float instead of int
+ */
+ */
+ */
 int a = 3.5;
 int b = 11.25;
 
