20dead7384951790c436796c8763b82f3671e8d9
442002918 <91384891+442002918@users.noreply.github.com>
2021-12-17 21:50:32 +0300

    Create powFunction.java

---
+++ powFunction.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+public class Main {
+public static void main(String args[]) {
+double x = 60;
+double y = 3;
+System.out.println(Math.pow(x, y));
+x = 3;
+y = 4;
+System.out.println(Math.pow(x, y));
+x = 2;
+y = 5;
+System.out.println(Math.pow(x, y));
+}
+}
